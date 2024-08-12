8e500c18dc90bc46bbd01ca7b1f1c9c496ab665f
dodyvirgiawan <dody.virgiawan97@gmail.com>
2024-08-12 11:07:03 +0700

    fix typo in webpack prod for container

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -28,7 +28,7 @@ const prodConfig = {
       remotes: {
         // Since this is a production, we need to specify where each MFE will be deployed to
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth${domain}/auth/latest/remoteEntry.js`
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`
       },
 
       shared: {
